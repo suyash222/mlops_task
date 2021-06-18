ac660b4892850d6b3b195bbb69b1ddf6e0d5be49
suyash222 <43377834+suyash222@users.noreply.github.com>
2021-06-18 18:13:32 +0530

    Add files via upload

---
+++ task 6/face_trainer.py
@@ -0,0 +1,78 @@
+import cv2
+import numpy as np
+from os import listdir, makedirs
+from os.path import isfile, join
+
+#name of the file
+model_name = 'suyash.xml'
+
+try:
+    makedirs('faces\user')
+except:
+    pass
+
+face_classifier = cv2.CascadeClassifier('face.xml')
+
+def face_extractor(img):
+
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+
+    if faces is ():
+        return None
+
+    for (x,y,w,h) in faces:
+        cropped_face = img[y:y+h, x:x+w]
+
+    return cropped_face
+
+cap = cv2.VideoCapture(0)
+count = 0
+
+while True:
+
+    ret, frame = cap.read()
+    if face_extractor(frame) is not None:
+        count += 1
+        face = cv2.resize(face_extractor(frame), (200, 200))
+        face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+
+        # Save file in specified directory with unique name
+        file_name_path = 'faces/user/' + str(count) + '.jpg'
+        cv2.imwrite(file_name_path, face)
+
+        # Put count on images and display live count
+        cv2.putText(face, str(count), (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 2)
+        cv2.imshow('Face Cropper', face)
+
+    else:
+        print("Face not found")
+        pass
+
+    if cv2.waitKey(1) == 13 or count == 100: #13 is the Enter Key
+        break
+
+cv2.destroyAllWindows()
+print("Collecting Samples Complete")
+
+data_path = './faces/user/'
+onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path, f))]
+
+Training_Data, Labels = [], []
+
+for i, files in enumerate(onlyfiles):
+    image_path = data_path + onlyfiles[i]
+    images = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    Training_Data.append(np.asarray(images, dtype=np.uint8))
+    Labels.append(i)
+
+Labels = np.asarray(Labels, dtype=np.int32)
+
+model  = cv2.face_LBPHFaceRecognizer.create()
+model.train(np.asarray(Training_Data), np.asarray(Labels))
+model.save(model_name)
+
+print("Model trained successfully")
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ task 6/task_6.py
@@ -0,0 +1,115 @@
+from smtplib import SMTP_SSL
+from email.message import EmailMessage
+from openwa import WhatsAPIDriver
+import os, json, time, subprocess, cv2
+
+my_server = SMTP_SSL(host='smtp.gmail.com')
+
+# list required of vaiable for email
+
+password = 'google@Suyash1234'
+username = 'suyash98garg@gmail.com'
+
+send_to = 'suyash98garg@gmail.com'
+send_by = 'suyash98garg@gmail.com'
+
+body = 'Welcome Back'
+subject = 'Welcome'
+
+# list required variable for whatsapp
+
+# enter the phone number of the person you want to send the message
+contact_number = '7742385251'
+message = 'Welcome Back'
+
+# model parameter
+model_to_use = 'suyash.xml'
+
+
+def send_email():
+    msg = EmailMessage()
+    msg['Subject'] = subject
+    msg['From'] = send_by
+    msg['To'] = send_to
+    msg.set_content(body)
+    my_server.login(username, password)
+    my_server.send_message(msg)
+
+
+def send_whatsapp():
+    os.environ['SELENIUM'] = 'chromedriver.exe'
+    driver = WhatsAPIDriver(client='chrome')
+    driver.wait_for_login()
+    dd = driver.get_chat_from_phone_number(contact_number)
+    dd.send_message(message)
+
+
+def create_ec2():
+
+    ec2 = json.loads(subprocess.getoutput('aws ec2 run-instances --image-id ami-0ad704c126371a549 --instance-type t2.micro --subnet-id subnet-7a2a2312'))
+
+    ec2_id  = ec2['Instances'][0]['InstanceId']
+
+    ebs = json.loads(subprocess.getoutput('aws ec2 create-volume --availability-zone ap-south-1a --size 2'))
+    ebs_id = ebs['VolumeId']
+
+    time.sleep(30)
+
+    _ = subprocess.getoutput(f'aws ec2 attach-volume --device /dev/sdh --instance-id {ec2_id} --volume-id {ebs_id}')
+
+
+face_classifier = cv2.CascadeClassifier('face.xml')
+suyash_model = cv2.face_LBPHFaceRecognizer.create()
+suyash_model.read('suyash.xml')
+
+
+def face_detector(img, size=0.5):
+
+    # Convert image to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+    if faces is ():
+        return img, []
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 255), 2)
+        roi = img[y:y+h, x:x+w]
+        roi = cv2.resize(roi, (200, 200))
+    return img, roi
+
+
+# Open Webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+
+    try:
+        _, frame = cap.read()
+        image, face = face_detector(frame)
+
+        face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+        results = suyash_model.predict(face)
+
+        if results[1] < 500:
+            confidence = int(100 * (1 - (results[1])/400))
+
+        if confidence > 85:
+            send_email()
+            print('email sended')
+            send_whatsapp()
+            print('whats message sent')
+            create_ec2()
+            print('ec2 create and attached')
+            break
+
+        if cv2.waitKey(1) == 13:  # 13 is the Enter Key
+            break
+
+    except Exception as e:
+        cv2.putText(image, "No Face Found", (220, 120),
+                    cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+        cv2.imshow('Face Recognition', image)
+        print(e)
+
+cap.release()
+cv2.destroyAllWindows()
